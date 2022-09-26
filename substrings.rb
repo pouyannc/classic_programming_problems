aa5e2ec5426a7f210db247c34072cb89412f7275
Pouyan Chamanian <pouyannc@gmail.com>
2022-09-26 19:19:48 -0400

    Simplest case of substrings method

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,8 @@
 def substrings (word, dictionary)
-    
+    substring_h = dictionary.each_with_object(Hash.new(0)) do |substring, substring_h|
+        substring_h[substring] += 1 if word.include?(substring)
+    end
+    substring_h
 end
+
+p substrings("hi there", ["hi", "hello", "h", "there"])
