2417d34243df430e5f3cbe69e47c8602ac72c93f
Pouyan <pouyannc@gmail.com>
2022-09-26 17:31:16 -0400

    Working caeser cipher but missing some conditions Capital letters do not wrap back. Large shift factors are not reduced.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,21 @@
-def caeser_cipher(string, shift) do #return string modified by shifting each letter by shift factor
-    
+def caeser_cipher(string, shift) #return string modified by shifting each letter by shift factor
+    #make sure to reduce shift by 25 unless its below 26 + some other condition
+    modified_string_arr = string.split("").map do |c|
+        if [*("A".."z")].any?(c)
+            if c.ord + 5 > 122 
+                (c.ord + 5 - 26).chr
+            else
+                (c.ord + 5).chr
+            end
+        else 
+            c
+        end
+    end
+    modified_string = modified_string_arr.join
+    p modified_string
 end
+
+caeser_cipher("Hello, world!", 5)
+
+#a.ord => 97
+#A.ord => 65
