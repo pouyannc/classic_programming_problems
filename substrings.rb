aab64d57a7a85960edec5b974a877265d7bc43cc
Pouyan <pouyannc@gmail.com>
2022-09-26 19:47:12 -0400

    Working substring method

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,11 @@
-def substrings (word, dictionary)
+def substrings (string, dictionary)
+    string.downcase!
     substring_h = dictionary.each_with_object(Hash.new(0)) do |substring, substring_h|
-        substring_h[substring] += 1 if word.include?(substring)
+        substring_h[substring] += string.scan(substring.downcase).count if string.include?(substring.downcase)
     end
     substring_h
 end
 
-p substrings("hi there", ["hi", "hello", "h", "there"])
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
