c048d5ff285659caa41bd9bcb74ee8d82b47b39a
Pouyan <pouyannc@gmail.com>
2022-09-26 20:34:04 -0400

    Working bubble sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,18 @@
 def swap_elements (array, i, j)
     array[i], array[j] = array[j], array[i]
+    array
 end
 
 def bubble_sort (array)
     final_index = array.length-1
-    (0..final_index-1).each do |i|
-        swap_elements(array, i, i+1) if array[i] > array[i+1]
+    sorted = 0
+    until sorted == final_index do
+        (0..final_index-sorted-1).each do |i|
+            array = swap_elements(array, i, i+1) if array[i] > array[i+1]
+        end
+        sorted += 1
     end
+    array
 end
 
-bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2])
